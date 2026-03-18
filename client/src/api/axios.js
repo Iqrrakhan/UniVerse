1966086a69f4fca63d203776888ddfa4001107c8
Iqra Khan <2705ikhan@gmail.com>
2026-03-19 02:39:28 +0500

    Fix axios URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://your-render-url.onrender.com/api'
+  baseURL: 'https://universe-api.onrender.com/api'  // ← your ACTUAL Render URL
 })
 
 export default instance
